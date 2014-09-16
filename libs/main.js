58bfcf03084f2beb5d3df8b2b704de6cbdb16a5a
Felipe Genuino <design@felipegenuino.com>
2014-09-16 02:50:44 -0300

    Update animacao

--- libs/main.js
+++ libs/main.js
@@ -13,16 +13,44 @@ $(document).ready(function() {
  				navigationPosition: 'right',
 				navigationTooltips: ['Home', 'Você pode',  'Incentivos', 'Por que é difícil investir',  'Conheça', 'Custo Zero', 'dot-beneficios',  'dot-como-funciona',  'dot-receber', 'dot-analisar',  'dot-financiar',  'dot-acompanhar',  'dot-contato',  'dot-apoio' ],
 			
-				onLeave: function(index, nextIndex, direction){ },
-		        afterLoad: function(anchorLink, index){ /*quando carrega pagina alert("onLoad"); */    },
+				onLeave: function(index, nextIndex, direction){ 
+ 
+						 if(index == '1'){ 
+						 	$('.imagem-voce-pode').addClass('delay-1s animated fadeInLeftBig'); 
+						 	$('.enfase-large').addClass('delay-2s animated fadeInRightBig');   
+						 	$('.texto-medium').addClass('delay-3s animated fadeInRightBig');
+						 	$('.block-medium').addClass('delay-4s animated fadeInRightBig');
+						 }
+
+						 if(index == '2'){ 
+						 	$('.imagem-incentivos').addClass('delay-1s animated fadeInDownBig'); 
+						 	$('.box-bottom').addClass('delay-3s animated fadeInUpBig');   
+  						 }
+
+  						 if(index == '3'){ 
+  						 	$('.imagem-dificil-investir').addClass('animated fadeInRightBig'); 
+ 						 	$('.group-titile').addClass('delay-1s animated fadeInDownBig'); 
+						 	$('.lista-1').addClass('delay-3s animated fadeInLeftBig');  
+						 	$('.lista-2').addClass('delay-4s animated fadeInLeftBig');   
+						 	$('.lista-3').addClass('delay-5s animated fadeInLeftBig');   
+						 	$('.lista-4').addClass('delay-6s animated fadeInLeftBig');    
+						 	$('.lista-5').addClass('delay-7s animated fadeInLeftBig'); 
+  						 }
+
+
+				},
+		        afterLoad: function(anchorLink, index){ /*quando carrega pagina alert("onLoad"); */  
+								 
+		          },
 		        afterRender: function(){ /*alert("The resulting DOM stucture is ready"); */ 	      
-		        	$('.info-section0').addClass('animated fadeUpRight'); 
-		         },
+ 		        	 $('.loader').hide();
+		        	 $('.header').addClass('animated fadeInDownBig'); 
+
+
+ 		         },
 		        afterResize: function(){ /*alert("The sections have finished resizing"); */		       },
 		        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){  },
-		        onSlideLeave: function(anchorLink, index, slideIndex, direction){
-		        	alert("afterSlideLeave");
- 		        }
+		        onSlideLeave: function(anchorLink, index, slideIndex, direction){ }
  
 
 

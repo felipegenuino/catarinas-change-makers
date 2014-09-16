baeabb908fbdf6ff638502283d28459908fa9f79
Felipe Genuino <design@felipegenuino.com>
2014-09-16 00:43:50 -0300

    update commit

--- libs/main.js
+++ libs/main.js
@@ -1,15 +1,42 @@
+ 
 ////// jQuery FullPage ////////
 $(document).ready(function() {
 			$('#fullpage').fullpage({
-                 anchors: ['home', 'voce-pode',  'investir', 'conheca',  'custo-zero',  'beneficios',  'como-funciona',  'receber', 'analisar',  'financiar',  'acompanhar',  'contato',  'apoio' ],
+                 anchors: ['home','voce-pode','incentivos','dificuldade','conheca', 'custo-zero', 'beneficios',  'como-funciona',  'receber', 'analisar',  'financiar',  'acompanhar',  'contato',  'apoio' ],
 				// sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
 				 menu: '#main-menu',
  				navigation: true,
-						paddingTop: '3em',
-		css3: true,
-				'verticalCentered': false,
+				paddingTop: '50px',
+				css3: false,
+ 				 //autoScrolling: false,
+ 				 //fixedElements: '.imagem-voce-pode',
+ 				navigationPosition: 'right',
+				navigationTooltips: ['Home', 'Você pode',  'Incentivos', 'Por que é difícil investir',  'Conheça', 'Custo Zero', 'dot-beneficios',  'dot-como-funciona',  'dot-receber', 'dot-analisar',  'dot-financiar',  'dot-acompanhar',  'dot-contato',  'dot-apoio' ],
+			
+				onLeave: function(index, nextIndex, direction){ },
+		        afterLoad: function(anchorLink, index){ /*quando carrega pagina alert("onLoad"); */    },
+		        afterRender: function(){ /*alert("The resulting DOM stucture is ready"); */ 	      
+		        	$('.info-section0').addClass('animated fadeUpRight'); 
+		         },
+		        afterResize: function(){ /*alert("The sections have finished resizing"); */		       },
+		        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){  },
+		        onSlideLeave: function(anchorLink, index, slideIndex, direction){
+		        	alert("afterSlideLeave");
+ 		        }
+ 
 
-				navigationPosition: 'right',
-				navigationTooltips: ['dot-home', 'dot-voce-pode',  'dot-investir', 'dot-conheca',  'dot-custo-zero',  'dot-beneficios',  'dot-como-funciona',  'dot-receber', 'dot-analisar',  'dot-financiar',  'dot-acompanhar',  'dot-contato',  'dot-apoio' ]
+
 			});
 		});
+
+
+
+
+ 
+
+
+
+
+ 
+
+ 

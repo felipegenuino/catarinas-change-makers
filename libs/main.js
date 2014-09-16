ceebafd654df230016f769f458bcadfda663ada3
Felipe Genuino <design@felipegenuino.com>
2014-09-16 10:49:20 -0300

    Update animation

--- libs/main.js
+++ libs/main.js
@@ -15,32 +15,59 @@ $(document).ready(function() {
 			
 				onLeave: function(index, nextIndex, direction){ 
  
+
+						 if(index == '0'){ 
+						 	$('#setion0 .animation-1').addClass('delay-1s animated fadeInLeftBig'); 
+						 	$('#setion0 .animation-2').addClass('delay-2s animated fadeInLeftBig');   
+						 	$('#setion0 .animation-3').addClass('delay-3s animated fadeInLeftBig');
+ 						 }
+
 						 if(index == '1'){ 
-						 	$('.imagem-voce-pode').addClass('delay-1s animated fadeInLeftBig'); 
-						 	$('.enfase-large').addClass('delay-2s animated fadeInRightBig');   
-						 	$('.texto-medium').addClass('delay-3s animated fadeInRightBig');
-						 	$('.block-medium').addClass('delay-4s animated fadeInRightBig');
+						 	$('#section1 .animation-1').addClass('         animated fadeInLeftBig'); 
+						 	$('#section1 .animation-2').addClass('delay-1s animated fadeInRightBig');   
+						 	$('#section1 .animation-3').addClass('delay-2s animated fadeInRightBig');
+						 	$('#section1 .animation-4').addClass('delay-3s animated fadeInRightBig');
 						 }
 
 						 if(index == '2'){ 
-						 	$('.imagem-incentivos').addClass('delay-1s animated fadeInDownBig'); 
-						 	$('.box-bottom').addClass('delay-3s animated fadeInUpBig');   
+						 	$('#section2 .animation-1').addClass('delay-1s animated fadeInDownBig'); 
+						 	$('#section2 .animation-2').addClass('delay-3s animated fadeInUpBig');   
   						 }
 
   						 if(index == '3'){ 
-  						 	$('.imagem-dificil-investir').addClass('animated fadeInRightBig'); 
- 						 	$('.group-titile').addClass('delay-1s animated fadeInDownBig'); 
-						 	$('.lista-1').addClass('delay-3s animated fadeInLeftBig');  
-						 	$('.lista-2').addClass('delay-4s animated fadeInLeftBig');   
-						 	$('.lista-3').addClass('delay-5s animated fadeInLeftBig');   
-						 	$('.lista-4').addClass('delay-6s animated fadeInLeftBig');    
-						 	$('.lista-5').addClass('delay-7s animated fadeInLeftBig'); 
+  						 	$('#section3 .animation-1').addClass('animated fadeInRightBig'); 
+ 						 	$('#section3 .animation-2').addClass('delay-1s animated fadeInDownBig'); 
+						 	$('#section3 .animation-3').addClass('delay-3s animated fadeInLeftBig');  
+						 	$('#section3 .animation-4').addClass('delay-4s animated fadeInLeftBig');   
+						 	$('#section3 .animation-5').addClass('delay-5s animated fadeInLeftBig');   
+						 	$('#section3 .animation-6').addClass('delay-6s animated fadeInLeftBig');    
+						 	$('#section3 .animation-7').addClass('delay-7s animated fadeInLeftBig'); 
+  						 }
+
+  						 if(index == '4'){ 
+  						 	$('#section4 .animation-1') .addClass('delay-1s  animated fadeInRightBig'); 
+  						 	$('#section4 .animation-2') .addClass('delay-2s  animated fadeInLeftBig'); 
+ 						 	$('#section4 .animation-3') .addClass('delay-3s  animated fadeInLeftBig'); 
+						 	$('#section4 .animation-4') .addClass('delay-4s  animated fadeInLeftBig');  
+						 	$('#section4 .animation-5') .addClass('delay-5s  animated fadeInUpBig');   
+						 	$('#section4 .animation-6') .addClass('delay-6s  animated fadeInUpBig');    
+						 	$('#section4 .animation-7') .addClass('delay-7s  animated fadeInUpBig'); 
+						 	$('#section4 .animation-8') .addClass('delay-8s  animated fadeInUpBig');   
+						 	$('#section4 .animation-9') .addClass('delay-9s  animated fadeInUpBig');    
+						 	$('#section4 .animation-10').addClass('delay-10s animated fadeInUpBig'); 
+						 	$('#section4 .animation-11').addClass('delay-11s animated fadeInUpBig');   
+						 	$('#section4 .animation-12').addClass('delay-12s animated fadeInUpBig');    
+						 	$('#section4 .animation-13').addClass('delay-13s animated fadeInUpBig'); 
   						 }
 
 
 				},
 		        afterLoad: function(anchorLink, index){ /*quando carrega pagina alert("onLoad"); */  
-								 
+						  
+
+						 
+
+
 		          },
 		        afterRender: function(){ /*alert("The resulting DOM stucture is ready"); */ 	      
  		        	 $('.loader').hide();

d4ad6e21935a801e11a6c1583550ef8d1ae369b6
Felipe Genuino <design@felipegenuino.com>
2014-09-18 16:54:45 -0300

    Ajuste no tempo dos slides 4, 9, 10, 11 e 12
    ·         Fazer o “surgimento” dos itens e informações mais lento nos
    slides 4, 9, 10, 11 e 12

--- libs/main.js
+++ libs/main.js
@@ -84,7 +84,7 @@ $(document).ready(function() {
 						 	$('#section7 .animation-8').addClass('delay-4s animated fadeInLeft');
    						 }
 
-  						 if(index == '8'){ 
+  						 if(index == '8'){ /* 1 receber */
 						 	$('#section8 .animation-1').addClass('animated fadeInRightBig'); 
 						 	$('#section8 .animation-2').addClass('delay-1s animated zoomIn');
 						 	$('#section8 .animation-3').addClass('delay-2s animated fadeInRightBig');
@@ -93,34 +93,34 @@ $(document).ready(function() {
 						 	$('#section8 .animation-6').addClass('delay-8s animated fadeInUpBig');   
   						 }
 
-  						 if(index == '9'){ 
+  						 if(index == '9'){ /* 2 Analisar */
 						 	$('#section9 .animation-1').addClass('animated fadeInLeftBig'); 
 						 	$('#section9 .animation-2').addClass('delay-1s animated zoomIn');
 						 	$('#section9 .animation-3').addClass('delay-2s animated fadeInRightBig');
-						 	$('#section9 .animation-4').addClass('delay-3s animated fadeInUpBig');
-						 	$('#section9 .animation-5').addClass('delay-4s animated fadeInLeftBig');
-						 	$('#section9 .animation-6').addClass('delay-5s animated fadeInUpBig');     
+						 	$('#section9 .animation-4').addClass('delay-4s animated fadeInUpBig');
+						 	$('#section9 .animation-5').addClass('delay-6s animated fadeInLeftBig');
+						 	$('#section9 .animation-6').addClass('delay-8s animated fadeInUpBig');     
   						 }
 
-  						 if(index == '10'){ 
+  						 if(index == '10'){ /* 3 Financiar */
 						 	$('#section10 .animation-1').addClass('animated fadeInLeftBig'); 
 						 	$('#section10 .animation-2').addClass('delay-1s animated zoomIn');
 						 	$('#section10 .animation-3').addClass('delay-2s animated fadeInRightBig');
-						 	$('#section10 .animation-4').addClass('delay-3s animated fadeInUpBig');
-						 	$('#section10 .animation-5').addClass('delay-4s animated fadeInLeftBig');
-						 	$('#section10 .animation-6').addClass('delay-5s animated fadeInUpBig');   
+						 	$('#section10 .animation-4').addClass('delay-4s animated fadeInUpBig');
+						 	$('#section10 .animation-5').addClass('delay-6s animated fadeInLeftBig');
+						 	$('#section10 .animation-6').addClass('delay-8s animated fadeInUpBig');   
   						 }
 
-  						 if(index == '11'){ 
+  						 if(index == '11'){ /* 4 Acompanhar */
 						 	$('#section11 .animation-1').addClass('animated fadeInRightBig'); 
 						 	$('#section11 .animation-2').addClass('delay-1s animated zoomIn');
 						 	$('#section11 .animation-3').addClass('delay-2s animated fadeInRightBig');
-						 	$('#section11 .animation-4').addClass('delay-3s animated fadeInUpBig');
-						 	$('#section11 .animation-5').addClass('delay-4s animated fadeInLeftBig');
-						 	$('#section11 .animation-6').addClass('delay-5s animated fadeInUpBig');    
+						 	$('#section11 .animation-4').addClass('delay-4s animated fadeInUpBig');
+						 	$('#section11 .animation-5').addClass('delay-6s animated fadeInLeftBig');
+						 	$('#section11 .animation-6').addClass('delay-8s animated fadeInUpBig');    
   						 }
 
-  						 if(index == '12'){ 
+  						 if(index == '12'){ /* Contato */
 						 	$('#section12 .animation-1').addClass('animated fadeInUpBig'); 
 						 	$('#section12 .animation-2').addClass('delay-1s animated fadeInLeftBig');
 						 	$('#section12 .animation-3').addClass('delay-2s animated fadeInUp');
@@ -130,7 +130,7 @@ $(document).ready(function() {
   						 }
 
 
-  						 if(index == '13'){ 
+  						 if(index == '13'){ /* Apoio */
 						 	$('#section13 .animation-1').addClass('animated fadeInDownBig'); 
 						 	$('#section13 .animation-2').addClass('delay-1s animated fadeInUpBig');   
   						 }
